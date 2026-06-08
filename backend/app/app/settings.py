b65aac349308ce2f2916eee199979b05a128dce4
Pedro Dias <dias.contatodev@gmail.com>
2026-06-08 19:55:55 -0300

    feature: Adicionando a camada de frontend preparada para receber a api do backend

--- backend/app/app/settings.py
+++ backend/app/app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5*47xm13$qlcb8s40he0d5h0di%xvcrui0mzm53(pqvr+@#qgx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
